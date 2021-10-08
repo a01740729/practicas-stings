ab403b9d87301476f89a86fc729a5236c3354ceb
a01740729 <a01740729@itesm.mx>
2021-10-08 14:06:58 +0000

    strings

---
+++ assignments/00Longitud/src/exercise.py
@@ -0,0 +1,15 @@
+def main():
+    #escribe tu código abajo de esta línea
+    pass
+    s1 = input ('cadena 1: ')
+    s2 = input ('cadena 2: ')
+
+    if len(s1) == len(s2):
+        print (s1)
+        print (s2)
+    elif len(s1) > len(s2):
+        print (s1)
+    else:
+        print (s2)
+if __name__=='__main__':
+    main()
--- assignments/01Fragmentos/src/exercise.py
+++ assignments/01Fragmentos/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+   s = input('>>>')
+
+   print (len(s))
+   print(s[0])
+   print(s[-1])
+   print(s[1::2])
+   
 
 if __name__=='__main__':
     main()
